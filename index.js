9805b8f8b234bc05b126531626cc0677aa0eb875
naveenira <naveen.r@tringapps.com>
2019-11-11 15:11:23 +0530

    1) Simply add a js method and implementations in Node.js

---
+++ index.js
@@ -0,0 +1,14 @@
+var rect = {
+    perimeter: (x, y) => (2*(x+y)),
+    area: (x,y) => (x*y)
+};
+
+function resolveRectanangles(l, m) {
+    console.log("l="+l+" m="+m);
+    console.log("Area of the rectangle: "+rect.area(l,m));
+    console.log("Perimeter of the reactangle: "+rect.perimeter(l,m));
+}
+
+
+resolveRectanangles(2,3);
+resolveRectanangles(10,3);
